b03ed0bfd7f3b05fb3955467301a53bdd9ab7fec
Francesca <36923806+FrancescaBambozzi@users.noreply.github.com>
2024-07-26 17:13:03 +0200

    added hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* COOKIES ALERT LOGIC */
 document.addEventListener("DOMContentLoaded", function() {
     const cookieConsent = document.getElementById('cookie-consent');
     const acceptCookiesBtn = document.getElementById('accept-cookies');
@@ -11,5 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
     acceptCookiesBtn.addEventListener('click', function() {
         localStorage.setItem('cookiesAccepted', 'true');
         cookieConsent.style.display = 'none';
+    });
+});
+
+/* HAMBURGER MENU */
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const mainNav = document.getElementById('main-nav');
+    
+    hamburgerMenu.addEventListener('click', () => {
+        mainNav.classList.toggle('active');
     });
 });
